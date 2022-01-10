85f6d217d40a2778da52418362206229488fc976
thanawat.bcr <thanwat.bcr@gmail.com>
2022-01-11 06:08:19 +0700

    [ADD] color system

--- src/App.vue
+++ src/App.vue
@@ -16,23 +16,25 @@ export default App;
 // .app {}
 // SETUP PRIMARY AND SECONDARY COLOR HERE
 :root {
-  --color-primary-100: #E6FAF4;
-  --color-primary-200: #BFF4E3;
-  --color-primary-300: #99EDD3;
-  --color-primary-400: #4DDFB1;
-  --color-primary-500: #00D190;
-  --color-primary-600: #00BC82;
-  --color-primary-700: #007D56;
-  --color-primary-800: #005E41;
-  --color-primary-900: #003F2B;
-  --color-secondary-100: #E9EFF9;
-  --color-secondary-200: #C7D7F1;
-  --color-secondary-300: #A5BFE8;
-  --color-secondary-400: #6290D7;
-  --color-secondary-500: #1F60C6;
-  --color-secondary-600: #1C56B2;
-  --color-secondary-700: #133A77;
-  --color-secondary-800: #0E2B59;
-  --color-secondary-900: #091D3B;
+  // 900 / 500
+  --color-primary-100: #989386;
+  --color-primary-200: #8E897B;
+  --color-primary-300: #797567;
+  --color-primary-400: #635F55;
+  --color-primary-500: #4D4A42;
+  --color-primary-600: #37352F;
+  --color-primary-700: #191815;
+  --color-primary-800: #000000;
+  --color-primary-900: #000000;
+
+  // --color-secondary-100: #E9EFF9;
+  // --color-secondary-200: #C7D7F1;
+  // --color-secondary-300: #A5BFE8;
+  // --color-secondary-400: #6290D7;
+  // --color-secondary-500: #1F60C6;
+  // --color-secondary-600: #1C56B2;
+  // --color-secondary-700: #133A77;
+  // --color-secondary-800: #0E2B59;
+  // --color-secondary-900: #091D3B;
 }
 </style>
