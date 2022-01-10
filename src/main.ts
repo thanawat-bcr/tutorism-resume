37d08c6284c9ae78a02c0ff7f3bc36524a996834
thanawat-bcr <thanawat.bcr@gmail.com>
2022-01-11 06:01:13 +0700

    [INIT]

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueCompositionAPI from '@vue/composition-api';
+
+import App from './App.vue';
+import router from './router';
+import './assets/tailwind.css';
+
+Vue.use(VueCompositionAPI);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
